ee02c67bfde0bfd65054189454f3f87e7cfdf299
Ayushi <aayushijain2702@gmail.com>
2023-01-19 21:55:32 +0530

    Refactor : Extract method reset game board to a separate method.

--- TicTacToe2/src/main/java/com/tictactoe/game/service/GameService.java
+++ TicTacToe2/src/main/java/com/tictactoe/game/service/GameService.java
@@ -72,14 +72,17 @@ public class GameService {
 
 	private GameResponse validateGameAndSendResponse(Player player) {
 		if (isWinnerAvailable()) {
-			previousPlayer = ZERO;
-			gameBoard.initializeGameBoard();
+			resetGameState();
 			return new GameResponse(String.format("Player %s won the game", player), "GAME_OVER");
 		} else if (gameBoard.isBoardFull()) {
-			previousPlayer = ZERO;
-			gameBoard.initializeGameBoard();
+			resetGameState();
 			return new GameResponse("Game Draw", "GAME_OVER");
 		}
 		return new GameResponse("GAME_IN_PROGRESS", getNextPlayer(player), player);
+	}
+
+	private void resetGameState() {
+		previousPlayer = ZERO;
+		gameBoard.initializeGameBoard();
 	}
 }
