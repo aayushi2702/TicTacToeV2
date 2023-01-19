0aa117baa85f5ad2b8aa9a3fdbca5c0b8b5d7298
Ayushi <aayushijain2702@gmail.com>
2023-01-19 21:46:10 +0530

    Refactor: Extract game validation and send response from a separate method

--- TicTacToe2/src/main/java/com/tictactoe/game/service/GameService.java
+++ TicTacToe2/src/main/java/com/tictactoe/game/service/GameService.java
@@ -25,12 +25,7 @@ public class GameService {
 
 		validateCurrentTurn(player, position);
 		savePlayerOnBoard(player, position);
-		if (isWinnerAvailable()) {
-			return new GameResponse(String.format("Player %s won the game", player), "GAME_OVER");
-		} else if (gameBoard.isBoardFull()) {
-			return new GameResponse("Game Draw", "GAME_OVER");
-		}
-		return new GameResponse("GAME_IN_PROGRESS", getNextPlayer(player), player);
+		return validateGameAndSendResponse(player);
 	}
 
 	private boolean isPlayerO(Player player) {
@@ -70,5 +65,14 @@ public class GameService {
 		return gameBoard.isAnyOneOfThreeRowsOccupiedBySamePlayer()
 				|| gameBoard.isAnyOfThreeColumnsOccupiedBySamePlayer()
 				|| gameBoard.isAnyOfTwoDiagonalOccupiedBySamePlayer();
+	}
+
+	private GameResponse validateGameAndSendResponse(Player player) {
+		if (isWinnerAvailable()) {
+			return new GameResponse(String.format("Player %s won the game", player), "GAME_OVER");
+		} else if (gameBoard.isBoardFull()) {
+			return new GameResponse("Game Draw", "GAME_OVER");
+		}
+		return new GameResponse("GAME_IN_PROGRESS", getNextPlayer(player), player);
 	}
 }
