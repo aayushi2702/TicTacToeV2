9415682ed95143694eedb890bb6f8cfacadcdbb9
Ayushi <aayushijain2702@gmail.com>
2023-01-19 19:24:36 +0530

    chore : Implementing and Adding custom documentation in Swagger API

--- TicTacToe2/src/main/java/com/tictactoe/game/TicTacToeApplication.java
+++ TicTacToe2/src/main/java/com/tictactoe/game/TicTacToeApplication.java
@@ -3,7 +3,11 @@ package com.tictactoe.game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Tic Tac Toe", version = "1.0", description = "Tic Tac Toe Game"))
 public class TicTacToeApplication {
 
 	public static void main(String[] args) {
